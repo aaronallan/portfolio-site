0b1f961d24443e9bfd545d9a9f86b80f190c3a52
aaronallan <aaron.a.pollock@gmail.com>
2018-01-21 20:21:36 -0500

    fix avatar bug

--- js/functions.js
+++ js/functions.js
@@ -6,16 +6,16 @@ const nff = document.querySelector('.nff-link');
 const nffContainer = document.querySelector('.nff');
 const employerLink = document.querySelector('.employer-link');
 const avatar = document.querySelector('.filter');
-const djTrigger = document.querySelector('.dj-trigger');
+// const djTrigger = document.querySelector('.dj-trigger');
 
 function registerEventListeners() {
 	ycf.addEventListener('click', () => {
-		[ycfContainer, nffContainer, avatar, employerLink, djTrigger].map(el => el.classList.add('open'));
+		[ycfContainer, nffContainer, avatar, employerLink].map(el => el.classList.add('open'));
 	});
 
 	nff.addEventListener('click', () => {
 		[nffContainer, ycfContainer].map(el => el.classList.add('close'));
-		[employerLink, avatar, djTrigger].map(el => el.classList.remove('open'));
+		[employerLink, avatar].map(el => el.classList.remove('open'));
 
 	  setTimeout(() => {
 			[nffContainer, ycfContainer].map(el => el.classList.remove('close'));
@@ -23,10 +23,10 @@ function registerEventListeners() {
 	  }, 350);
 	});
 
-	djTrigger.addEventListener('mouseover', () => {
-		avatar.classList.add('rotate');
-		setTimeout(() => avatar.classList.remove('rotate'), 9000)
-	})
+	// djTrigger.addEventListener('mouseover', () => {
+	// 	avatar.classList.add('rotate');
+	// 	setTimeout(() => avatar.classList.remove('rotate'), 9000)
+	// })
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
